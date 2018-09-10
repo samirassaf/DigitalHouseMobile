eea8550c996eb9bda4dff67519348dcb930b1f48
samirassaf <samir_assaf_1@hotmail.com>
2018-09-10 20:28:18 -0300

    feito ligacao entre tela inicial e login

--- Android/Aula16/app/src/main/java/br/com/digitalhouse/app/SplashActivity.java
+++ Android/Aula16/app/src/main/java/br/com/digitalhouse/app/SplashActivity.java
@@ -1,14 +1,30 @@
 package br.com.digitalhouse.app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
+
+   private ImageView imagem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        imagem=(ImageView)findViewById(R.id.imageview_splash);
+        imagem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+
+            }
+        });
+
+
+
     }
 
     @Override
