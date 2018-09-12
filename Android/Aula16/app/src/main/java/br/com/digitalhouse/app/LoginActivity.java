34e1873a805cd5bbba74dc939aae4d858c163531
samirassaf <samir_assaf_1@hotmail.com>
2018-09-11 22:27:19 -0300

    aula 18 - cadastro

--- Android/Aula16/app/src/main/java/br/com/digitalhouse/app/LoginActivity.java
+++ Android/Aula16/app/src/main/java/br/com/digitalhouse/app/LoginActivity.java
@@ -1,13 +1,26 @@
 package br.com.digitalhouse.app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView textViewRegisterNow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        textViewRegisterNow=(TextView) findViewById(R.id.text_registernow);
+        textViewRegisterNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+            }
+        });
     }
 }
--- Android/Aula16/app/src/main/java/br/com/digitalhouse/app/SplashActivity.java
+++ Android/Aula16/app/src/main/java/br/com/digitalhouse/app/SplashActivity.java
@@ -10,6 +10,7 @@ public class SplashActivity extends AppCompatActivity {
 
    private ImageView imagem;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
